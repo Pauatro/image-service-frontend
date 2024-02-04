174a74a150a245a65af146b62c20a5a443f173b2
Pauatro <pauatro@gmail.com>
2024-02-04 23:21:23 +0100

    feat: add image api, logic and visual elements

---
+++ src/images/api/index.ts
@@ -0,0 +1,36 @@
+import { ApiMethod, fetchApi } from "../../shared/api";
+import { environmentService } from "../../shared/environmentService";
+import { DicomPixelData } from "../application/utils";
+
+interface GetImagePatientNameResponse {
+  name: string,
+}
+
+interface GetImagePixelArrayResponse {
+  pixel_array: DicomPixelData,
+}
+
+export class ImagesApi {
+  private static baseUrl = environmentService.backendBaseUrl;
+  private static imagesApiPrefix = "/images";
+  private static imagesApiEndpoints = {
+    patientName: `${this.baseUrl}${this.imagesApiPrefix}/patient`,
+    pixelArray: `${this.baseUrl}${this.imagesApiPrefix}/pixel-array`,
+  };
+
+  static async getImagePatientName() {
+    const data = await fetchApi<GetImagePatientNameResponse>({
+      url: this.imagesApiEndpoints.patientName,
+      method: ApiMethod.GET,
+    });
+    return data?.name??"";
+  }
+
+  static async getImagePixelArray() {
+    const data = await fetchApi<GetImagePixelArrayResponse>({
+      url: this.imagesApiEndpoints.pixelArray,
+      method: ApiMethod.GET,
+    });
+    return data?.pixel_array;
+  }
+}
---
+++ src/images/application/useImageData.tsx
@@ -0,0 +1,42 @@
+import { useState, useCallback, useRef } from "react";
+import { ImagesApi } from "../api";
+import { getClampedArrayFromDicomData } from "./utils";
+
+export const useImageData = () => {
+  const [patientName, setPatientName] = useState<string>("");
+  const canvasRef = useRef<HTMLCanvasElement>()
+
+  const getPatientName = useCallback(async () => {
+    const name = await ImagesApi.getImagePatientName();
+    setPatientName(name)
+  }, []);
+
+  const getImage = useCallback(async () => {
+    if (!canvasRef.current) return
+
+    const pixelData = await ImagesApi.getImagePixelArray();
+    if (!pixelData) return
+
+    const {
+        imageHeight, 
+        imageWidth, 
+        pixelArray
+    } = getClampedArrayFromDicomData(pixelData)
+
+    canvasRef.current.width = imageWidth
+    canvasRef.current.height = imageHeight
+
+    const imageDataInstance = new ImageData(pixelArray, imageHeight, imageWidth);
+    const ctx = canvasRef.current.getContext('2d');
+    ctx?.putImageData(imageDataInstance,0,0);
+
+  }, []);
+
+  return {
+    patientName,
+    setPatientName,
+    getPatientName,
+    getImage,
+    canvasRef,
+  };
+};
